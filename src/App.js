8711a08a2ca94609e20368fbc2569d0ffb4f56a4
ysfbgh <youssefboughanem55@gmail.com>
2023-11-19 03:05:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import ImageSlider from "./components/ImageSlider";
+import React from "react";
 
-function App() {
+const App = () => {
+  const slides = [
+    { URL: "/img/6512eb15010a6.jpeg", title: "jon jhones" },
+    { URL: "/img/16771948460814.jpg", title: "conor" },
+    { URL: "/img/maxresdefault (2).jpg", title: "khamzat" },
+    {
+      URL: "/img/new-mythical-fighter-world-peace-khamzat-chimaev-v0-gurzkptb0mvb1.webp",
+      title: "khabib",
+    },
+    { URL: "/img/skysports-khabib-nurmagomedov_5116546.jpg", title: "idk" },
+  ];
+
+  const containerStyles = {
+    width: "550px",
+    height: "450px",
+    margin: "0 auto",
+  };
+
+  const bodyStyles = {
+    background: "linear-gradient(to right, red, black)",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const h1Styles = {
+    color: "#fff",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={bodyStyles}>
+      <h1 style={h1Styles}>Slider</h1>
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
